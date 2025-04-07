e2d44a582dc183ce19e0e261bbdeb648678884dd
Blake <bsterling222@gmail.com>
2025-04-06 21:50:30 -0500

    Adding solutions section and buttons in solutions section - BS

--- app.js
+++ app.js
@@ -5,7 +5,15 @@ document.addEventListener('DOMContentLoaded', () => {
     const userInput = document.getElementById('user-initials');
     const loginSection = document.getElementById('login-section');
     const mainMenu = document.getElementById('main-menu');
-    const logoutButton = document.getElementById('logout-button')
+    const logoutButton = document.getElementById('logout-button');
+    const solutionsSection = document.getElementById('solutionsButton')
+    const RuHexSection = document.getElementById('RuHex-button')
+    const FerriSection = document.getElementById('Ferri-button')
+    const DASection = document.getElementById('DA-button')
+    const NESection = document.getElementById('NE-button')
+    const EPSection = document.getElementById('EP-button')
+    const AASection = document.getElementById('AA-button')
+    const newSolutionSection = document.getElementById('makeSolutionButton')
 
 // Login form Submission Handling
     loginForm.addEventListener('submit', (event) => {
@@ -21,12 +29,12 @@ document.addEventListener('DOMContentLoaded', () => {
 // Save Initials to sessionStorage for now
         sessionStorage.setItem('userInitials', initials);
 
-//Hide Login Menu and Show Main Menu
+// Hide Login Menu and Show Main Menu
         loginSection.style.display = "none";
         mainMenu.style.display = "block";  
     });
 
-//Logout Button Listener
+// Logout Button Listener
     logoutButton.addEventListener('click', () => {
 
 // Clears the stored initials
@@ -38,5 +46,53 @@ document.addEventListener('DOMContentLoaded', () => {
         
 // Clear Input
         userInput.value = "";
+    });
+
+// Solutions Section 
+    solutionsButton.addEventListener('click',() => {
+        solutions.style.display = "block";
+        mainMenu.style.display = 'none';
+    });
+    
+// RuHex Section 
+    RuHexSection.addEventListener('click',() => {
+        RuHexSection.style.display = "block";
+        solutionsSection.style.display = 'none';
+    });
+
+// Ferricyanide Section 
+    FerriSection.addEventListener('click',() => {
+        FerriSection.style.display = "block";
+        solutionsSection.style.display = 'none';
+    });
+
+// Dopamine Section 
+    DASection.addEventListener('click',() => {
+        DASection.style.display = "block";
+        solutionsSection.style.display = 'none';
+    });
+
+// Norepinephrine Section 
+    NESection.addEventListener('click',() => {
+        NESection.style.display = "block";
+        solutionsSection.style.display = 'none';
+    });
+
+// Epinephrine Section 
+    EPSection.addEventListener('click',() => {
+        EPSection.style.display = "block";
+        solutionsSection.style.display = 'none';
+    });
+
+// Ascorbic Acid Section 
+    AASection.addEventListener('click',() => {
+        AASection.style.display = "block";
+        solutionsSection.style.display = 'none';
+    });
+
+// New Solutions Section 
+    newSolutionSection.addEventListener('click',() => {
+        mainMenu.style.display = "block";
+        solutionsSection.style.display = 'none';
     });
 });
